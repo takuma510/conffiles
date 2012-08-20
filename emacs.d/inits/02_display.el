2c3e8f3f722f0a092e4d20b98d66e729aab381c2
takuma510 <takuma510@gmail.com>
2012-08-20 23:04:52 +0900

    modify display.el

--- emacs.d/inits/02_display.el
+++ emacs.d/inits/02_display.el
@@ -13,3 +13,42 @@
 (global-set-key (kbd "C-S-j") 'windmove-down)
 (global-set-key (kbd "C-S-k") 'windmove-up)
 (global-set-key (kbd "C-S-l") 'windmove-right)
+
+
+;=======================================================================
+; 全体
+;=======================================================================
+;; メニューバー、ツールバー、スクロールバーを消す, Emacs23以降
+(if (>= emacs-major-version 23)
+    (progn
+      (tool-bar-mode nil)
+      (scroll-bar-mode nil)
+      (menu-bar-mode nil)
+    )
+)
+
+
+;=======================================================================
+; モード行
+;=======================================================================
+;; 行番号・桁番号を表示
+(line-number-mode t)
+(column-number-mode t)
+
+
+;=======================================================================
+; 強調
+;=======================================================================
+;; 現在行をハイライト
+(global-hl-line-mode t)
+(defface my-hl-line-face
+  '((((class color) (background dark))  ; カラーかつ, 背景が dark ならば
+     (:background "DarkSlateBlue" t))   ; 背景を黒に.
+    (((class color) (background light)) ; カラーかつ, 背景が light でも
+     (:background "DarkSlateBlue" t))   ; 背景を黒に.
+    (t (:bold t)))
+  "hl-line's my face")
+(setq hl-line-face 'my-hl-line-face)
+
+;; 対応する括弧を表示させる
+(show-paren-mode t)
