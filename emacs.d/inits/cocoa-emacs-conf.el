e944675d8840a3ea51e43f41b21ca099978f87b4
takuma510 <takuma510@gmail.com>
2012-08-22 21:33:26 +0900

    add cocoa-emacs-conf.el

---
+++ emacs.d/inits/cocoa-emacs-conf.el
@@ -0,0 +1,9 @@
+;;
+;; Cocoa-emacs
+;;
+;=======================================================================
+; keybinds
+;=======================================================================
+;; swap command and option keys
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
