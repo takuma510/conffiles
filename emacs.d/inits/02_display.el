ddf82d1befcc87ba084680a001e95da66d765ed5
takuma510 <takuma510@gmail.com>
2012-08-25 07:50:00 +0900

    window-resizer

--- emacs.d/inits/02_display.el
+++ emacs.d/inits/02_display.el
@@ -30,6 +30,42 @@
 
 
 
+;;; ウィンドウ
+
+(defun window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        c)
+    (catch 'end-flag
+      (while t
+        (message "size[%dx%d]"
+                 (window-width) (window-height))
+        (setq c (read-char))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (message "Quit")
+               (throw 'end-flag t)))))))
+
+;; Ctrl-c R でウィンドウリサイズモード・hjklでリサイズ
+(global-set-key "\C-cR" 'window-resizer)
+
+
+
 ;;; モード行
 
 ;; 行番号・桁番号を表示
