f24dcda683b9fd4d52959278a42f62e1fbf4d133
takuma510 <takuma510@gmail.com>
2012-08-24 23:43:31 +0900

    refactoring org.el

--- emacs.d/inits/10_org.el
+++ emacs.d/inits/10_org.el
@@ -1,38 +1,50 @@
 
 ;;;;  org-mode
 
-;; Emacs23以降
-(if (>= emacs-major-version 23)
-    (progn
-      (require 'org)
-      (org-remember-insinuate)                                                  ; org-rememberの初期化
-      (setq org-directory "~/docs/")                                            ; メモを格納するorgファイルの設定
-      (setq org-default-notes-file (expand-file-name "memo.org" org-directory)) ; メモファイル
-      (setq org-remember-templates
-            '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
-              ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
-              ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")
-              ))
-      (global-set-key (kbd "M-m") 'org-remember)
 
-      ;; 要らないkey-bindingを無効化
-      (define-key org-mode-map [C-S-left]  nil)
-      (define-key org-mode-map [C-S-right] nil)
-      (define-key org-mode-map (kbd "C-,") nil)
-      (define-key org-mode-map [S-up]    nil)
-      (define-key org-mode-map [S-down]  nil)
-      (define-key org-mode-map [S-left]  nil)
-      (define-key org-mode-map [S-right] nil)
+(require 'org)
 
-      ;; TODO管理
-      (setq org-use-fast-todo-selection t)
-      (setq org-todo-keywords
-            '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(x)" "CANCEL(c)")
-              (sequence "APPT(a)" "|" "DONE(x)" "CANCEL(c)")))
+;;; org-remember
 
-      ;; 予定表
-      (setq org-default-notes-file "~/docs/memo.org") ; M-x org-remember によるメモを集めるorgファイル
-      (setq org-agenda-files (list org-default-notes-file)) ; 予定表に使うorgファイルのリスト
-      (global-set-key (kbd "C-c a") 'org-agenda) ; agenda閲覧
-    )
-)
+(global-set-key (kbd "M-m") 'org-remember) ; キーバインド
+(org-remember-insinuate) ; 初期化
+
+;; メモの格納場所
+(setq org-directory "~/docs/")
+(setq org-default-notes-file
+      (expand-file-name "memo.org" org-directory))
+
+;; テンプレート設定
+(setq org-remember-templates
+      '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
+	("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")))
+
+
+
+;;; TODO管理
+
+(setq org-use-fast-todo-selection t)
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(x)" "CANCEL(c)")
+	(sequence "APPT(a)" "|" "DONE(x)" "CANCEL(c)")))
+
+
+
+;;; 予定表
+
+;; 予定表に組み込まれるorgファイルのリスト
+(setq org-agenda-files (list org-default-notes-file))
+
+;; agenda閲覧
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+
+
+;;; 要らないkey-bindingを無効化
+(define-key org-mode-map [C-S-left]  nil)
+(define-key org-mode-map [C-S-right] nil)
+(define-key org-mode-map (kbd "C-,") nil)
+(define-key org-mode-map [S-up]    nil)
+(define-key org-mode-map [S-down]  nil)
+(define-key org-mode-map [S-left]  nil)
+(define-key org-mode-map [S-right] nil)
