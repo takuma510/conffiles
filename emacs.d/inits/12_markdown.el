1e36e2816f68d8d245959803e3d9134485083ae2
Takuma Goto <takuma510@gmail.com>
2012-09-14 07:53:15 +0900

    add markdown mode

---
+++ emacs.d/inits/12_markdown.el
@@ -0,0 +1,10 @@
+
+;;;; Markdown-mode 
+
+
+;;; default setting
+
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+      (cons '("\\.text" . markdown-mode) auto-mode-alist))
