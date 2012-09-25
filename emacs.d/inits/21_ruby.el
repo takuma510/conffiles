4b1dfdea2f00e4ee6565bd2c3fdfc95b64b4ea57
Takuma Goto <takuma510@gmail.com>
2012-09-25 12:31:54 +0900

    ruby-flymake

--- emacs.d/inits/21_ruby.el
+++ emacs.d/inits/21_ruby.el
@@ -36,11 +36,37 @@
 
 
 
+;;; flymake
+
+(require 'flymake)
+
+;; I don't like the default colors :)
+(set-face-background 'flymake-errline "red4")
+(set-face-background 'flymake-warnline "dark slate blue")
+
+;; Invoke ruby with '-c' to get syntax checking
+(defun flymake-ruby-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+	 (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+
+
+
+
 ;;; ruby-mode-hook
 
 (defun my-ruby-mode-hooks ()
   (inf-ruby-keys)
   (ruby-electric-mode t)
-  (ruby-block-mode t))
+  (ruby-block-mode t)
+  (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+      (flymake-mode)))
 
 (add-hook 'ruby-mode-hook 'my-ruby-mode-hooks)
