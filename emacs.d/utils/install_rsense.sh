d19bcbbfa9d8d203fd72c83dcc3a6c52c469fc7c
Takuma Goto <takuma510@gmail.com>
2012-09-25 18:39:20 +0900

    rm garbage file

--- emacs.d/utils/install_rsense.sh
+++ emacs.d/utils/install_rsense.sh
@@ -2,6 +2,7 @@
 
 RSENSE=rsense-0.3
 SOURCE_URL=http://cx4a.org/pub/rsense/${RSENSE}.tar.bz2
+SOURCE_FILE=`basename ${SOURCE_URL}`
 TMP_DIR=~/.emacs.d/tmp
 INSTALL_DIR=~/.emacs.d/manual-install
 
@@ -10,7 +11,9 @@ echo "install rsense to ${INSTALL_DIR}..."
 
 cd ${TMP_DIR}
 wget ${SOURCE_URL}
-tar -zxvf `basename ${SOURCE_URL}` -C ${INSTALL_DIR}
+tar -zxvf ${SOURCE_FILE} -C ${INSTALL_DIR}
+rm ${SOURCE_FILE}
+
 cd ${INSTALL_DIR}/${RSENSE}
 chmod +x bin/rsense
 ruby etc/config.rb > ~/.rsense
