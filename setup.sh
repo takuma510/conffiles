b61858ea44e3eff471b129e8af2dea6b6ddb9c8c
takuma510 <takuma510@gmail.com>
2012-09-29 23:09:55 +0900

    fix typo of setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,25 @@
 
 echo "setup conffiles..."
 
+echo "clean up links"
+rm ~/.zsh.d ~/.vim.d ~/.tmux.d ~/bin
+
 echo "setup ~/.zsh.d"
-ln -s conffiles/zsh.d ~/.zsh.d
+ln -s ~/conffiles/zsh.d ~/.zsh.d
 cd ~/.zsh.d/utils
 ./setup.sh
 
 echo "setup ~/.vim.d"
-ln -s conffiles/vim.d ~/.vim.d
+ln -s ~/conffiles/vim.d ~/.vim.d
 cd ~/.vim.d/utils
 ./setup.sh
 
 echo "setup ~/.tmux.d"
-ln -s confiles/tmux.d ~/.tmux.d
+ln -s ~/conffiles/tmux.d ~/.tmux.d
 cd ~/.tmux.d/utils
 ./setup.sh
 
 echo "setup ~/bin"
-ln -s conffiles/bin ~/bin
+ln -s ~/conffiles/bin ~/bin
 
 echo "done... \^_^/"
