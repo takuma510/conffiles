c6720535c3ac651780017ceab77a2558f22c7f65
Takuma Goto <takuma510@gmail.com>
2012-10-07 14:54:41 +0900

    add lispxmp

---
+++ emacs.d/inits/22_elisp.el
@@ -0,0 +1,35 @@
+;;; 22_elisp.el --- emacs lisp mode
+
+;; Copyright (C) 2012  後藤 拓馬
+
+;; Author: Takuma Goto
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(require 'lispxmp)
+
+;; C-c C-dと押すと註釈される
+(define-key emacs-lisp-mode-map (kbd "C-c C-d") 'lispxmp)
+
+
+
+(provide '22_elisp)
+;;; 22_elisp.el ends here
