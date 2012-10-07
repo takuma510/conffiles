9582ae835d4e35b65502e3870812ae961d1ea435
Takuma Goto <takuma510@gmail.com>
2012-10-07 14:36:57 +0900

    add open-junk-file

--- emacs.d/inits/04_file.el
+++ emacs.d/inits/04_file.el
@@ -78,6 +78,15 @@
 
 
 
+;;; open-junk-file
+
+(require 'open-junk-file)
+
+;; C-x C-zで試行錯誤用のファイルを開く
+(global-set-key (kbd "C-x C-z") 'open-junk-file)
+
+
+
 ;;; others
 
 ;; ファイルの先頭が #! で始まるファイルに実行権限をつける
