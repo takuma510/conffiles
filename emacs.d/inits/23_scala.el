7e97f7329fb22c0ed715c22cb27112e159d63ae2
Takuma Goto <takuma510@gmail.com>
2012-12-09 21:57:10 +0900

    install ENSIME

--- emacs.d/inits/23_scala.el
+++ emacs.d/inits/23_scala.el
@@ -2,3 +2,11 @@
 ;;;; scala-mode
 
 (require 'scala-mode-auto)
+
+
+;;; ENSIME(ENhanced Scala Interaction Mode for Emacs)
+
+(add-to-list 'load-path "~/.emacs.d/manual-install/ensime_2.9.2-0.9.8.1/elisp/")
+(require 'ensime)
+
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
