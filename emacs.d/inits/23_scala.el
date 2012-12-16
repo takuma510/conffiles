57d45d0bc2283d810895a2fe4afdcaad38fe21cb
Takuma Goto <takuma510@gmail.com>
2012-12-16 12:40:26 +0900

    add comment for using ENSIME

--- emacs.d/inits/23_scala.el
+++ emacs.d/inits/23_scala.el
@@ -5,6 +5,8 @@
 
 
 ;;; ENSIME(ENhanced Scala Interaction Mode for Emacs)
+;;; to create .ensime file, M-x ensime-config-gen
+;;; and set .ensime file using M-x ensime
 
 (add-to-list 'load-path "~/.emacs.d/manual-install/ensime_2.9.2-0.9.8.1/elisp/")
 (require 'ensime)
