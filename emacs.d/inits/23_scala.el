6753cf628bd1f707a1e79423ebea6a38d19a2c81
takuma510 <takuma510@gmail.com>
2012-12-23 16:27:09 +0900

    add comment for setup ENSIME on play framework

--- emacs.d/inits/23_scala.el
+++ emacs.d/inits/23_scala.el
@@ -7,6 +7,8 @@
 ;;; ENSIME(ENhanced Scala Interaction Mode for Emacs)
 ;;; to create .ensime file, M-x ensime-config-gen
 ;;; and set .ensime file using M-x ensime
+;;; if you use play framework, add 'addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "VERSION")'
+;;; to the plugins.sbt and ensime generate on play sbt :-)
 
 (add-to-list 'load-path "~/.emacs.d/manual-install/ensime_2.9.2-0.9.8.1/elisp/")
 (require 'ensime)
