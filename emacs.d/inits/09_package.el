23d2e1cb9004a474636e8ebd3301631a5ca16135
takuma510 <takuma510@gmail.com>
2013-07-08 17:38:44 +0900

    install slim-mode by package.el

--- emacs.d/inits/09_package.el
+++ emacs.d/inits/09_package.el
@@ -12,3 +12,23 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
+
+
+
+;; install pkg automatically (ref. http://qiita.com/catatsuy/items/5f1cd86e2522fd3384a0)
+
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    ;; ここに使っているパッケージを書く。
+    slim-mode
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
