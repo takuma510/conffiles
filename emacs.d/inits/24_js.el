42e7d8f2038319b19a1719fee0b1a86e87bdf6b6
Takuma Goto <takuma510@gmail.com>
2013-09-16 12:14:51 +0900

    Add generate-html5-project cmd for emacs

--- emacs.d/inits/24_js.el
+++ emacs.d/inits/24_js.el
@@ -3,3 +3,19 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (setq-default js2-basic-offset 2) ;; indent
+
+
+;;; generate html5 project
+(setq-default html5-project-directory "~/src/js")
+(setq-default html5-template "~/.emacs.d/project-template/html5/")
+
+(defun generate-html5-project (prj)
+  (interactive "sInput Project Name:")
+  (cd html5-project-directory)
+  (cond ((file-directory-p prj)
+         (message "Already created %S" prj))
+        (t
+         (copy-directory html5-template prj)
+         (find-file prj)
+         (message "Create %S project"
+                 (concat html5-project-directory "/" prj)))))
