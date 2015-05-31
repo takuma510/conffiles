f4ad52a66c0d8cb73a58ebdfd5b5863a22362626
Takuma Goto <takuma510@gmail.com>
2015-05-31 19:11:52 +0900

    remove rinari submodule

--- emacs.d/utils/install_submodule.sh
+++ emacs.d/utils/install_submodule.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
 CONFFILES=${HOME}/conffiles
-RINARI_PATH=${CONFFILES}/emacs.d/vender/rinari
 
 echo "install gitsubmodule..."
 
 cd ${CONFFILES}
-echo "cd dir `pwd`"
-git submodule init
-git submodule update
-cd ${RINARI_PATH}
 echo "cd dir `pwd`"
 git submodule init
 git submodule update
