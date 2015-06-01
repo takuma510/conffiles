ae2158682b5fb309fa25f12a9cf6f93a747f404b
Takuma Goto <takuma510@gmail.com>
2015-06-01 22:14:33 +0900

    robe auto-complete

--- emacs.d/inits/21_ruby.el
+++ emacs.d/inits/21_ruby.el
@@ -53,7 +53,9 @@
 
 (require 'robe)
 (add-hook 'ruby-mode-hook 'robe-mode)
-
+(autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
+(autoload 'ac-robe-setup "ac-robe" "auto-complete robe" nil nil)
+(add-hook 'robe-mode-hook 'ac-robe-setup)
 
 
 ;;; flymake
