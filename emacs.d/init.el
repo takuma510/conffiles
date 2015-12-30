246c8b3d81a05051d6b0505bca6c9a460929eab2
Takuma Goto <takuma510@gmail.com>
2015-12-30 12:58:36 +0900

    Use melpa package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,16 +1,86 @@
 
-;;;; 初めに読み込まれるファイル
+;;;; init
 
 
-;;; load-pathの追加
+;;; Add load-path
 
 (add-to-list 'load-path "~/.emacs.d/manual-install")
 
 
-;;; init-loader
+;;; Install packages
 
-(add-to-list 'load-path "~/.emacs.d/vender/init-loader")
+;; Usage
+;; M-x package-list-packages -> i(check install pkg), x(exec install)
+(require 'package)
+
+;; add package-archives server
+(add-to-list 'package-archives
+             '("melpa" .     "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; install pkg automatically (ref. http://qiita.com/catatsuy/items/5f1cd86e2522fd3384a0)
+(require 'cl)
+(defvar installing-package-list
+  '(
+    ;; ここに使っているパッケージを書く。
+    slim-mode
+    android-mode
+    php-mode
+    php-extras
+    flymake
+    flymake-php
+    flymake-cursor
+    flymake-easy
+    flymake-python-pyflakes
+    js2-mode
+    tss
+    ess
+    magit
+    haskell-mode
+    actionscript-mode
+    helm
+    popup
+    auto-complete
+    tabbar
+    viewer
+    auto-save-buffers-enhanced
+    session
+    lispxmp
+    dired-single
+    markdown-mode
+    yasnippet
+    rbenv
+    inf-ruby
+    ruby-electric
+    ruby-block
+    robe
+    paredit
+    igrep
+    open-junk-file
+    recentf-ext
+    py-autopep8
+    elpy
+    scala-mode2
+    ensime
+    go-mode
+    go-autocomplete
+    go-eldoc
+    undo-tree
+    init-loader
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
+
+
+;;; Load init-loader path
+
 (require 'init-loader)
-
-;; 設定ファイルがあるディレクトリを指定
 (init-loader-load "~/.emacs.d/inits")
