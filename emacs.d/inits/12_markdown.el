c85914000fcfb78ddf296ebf6430667220b82b22
Takuma Goto <takuma510@gmail.com>
2016-01-01 15:45:40 +0900

    Add open-atom kbd

--- emacs.d/inits/12_markdown.el
+++ emacs.d/inits/12_markdown.el
@@ -8,3 +8,6 @@
   "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
       (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+;; open-atom
+(define-key markdown-mode-map (kbd "C-o C-p") 'open-atom)
