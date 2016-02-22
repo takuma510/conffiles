755f6156737f8904ef76fb6f4a45df0cc1e34c5b
Takuma Goto <takuma510@gmail.com>
2016-02-22 12:34:56 +0900

    [Emacs] Refactoring 00_global.el

--- emacs.d/inits/00_global.el
+++ emacs.d/inits/00_global.el
@@ -4,48 +4,11 @@
 
 ;;; Code:
 
-;;; undo-tree
-
-(require 'undo-tree)
-(global-undo-tree-mode)
-
-
-
-;;; key map
-
-(define-key global-map [?¥] [?\\])  ;; ¥の代わりに\を入力する
-
-
-
-;;; kbd
-
-(global-unset-key "\C-t")
-(define-key global-map (kbd "C-c o") 'goto-line)
-
-
-
-;;; 文字コード
-
-(set-language-environment 'Japanese)
-(prefer-coding-system 'utf-8)
-
-
-
-;;; スタートアップ画面非表示
-
-(setq inhibit-startup-message t)
-
-
-
-;;; ジェネリックモード有効化
-
-(require 'generic-x)
-
-
 
 ;;; Elscreen
 
 ;; kbd
+(global-unset-key "\C-t")
 (setq elscreen-prefix-key (kbd "C-t"))
 (elscreen-start)
 
@@ -57,16 +20,34 @@
 (elscreen-persist-mode 1)
 
 
+;;; Others
 
-;;; PATH from $SHELL
+;; undo-tree
+(require 'undo-tree)
+(global-undo-tree-mode)
 
+;; kbd
+(define-key global-map (kbd "C-c o") 'goto-line)
+(define-key global-map [?¥] [?\\])  ;; ¥の代わりに\を入力する
+
+;; 文字コード
+(set-language-environment 'Japanese)
+(prefer-coding-system 'utf-8)
+
+;; スタートアップ画面非表示
+(setq inhibit-startup-message t)
+
+;; ジェネリックモード有効化
+(require 'generic-x)
+
+;; PATH from $SHELL
 (defun set-exec-path-from-shell-PATH ()
   "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-
-This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
   (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+  (let ((path-from-shell
+         (replace-regexp-in-string "[ \t\n]*$" ""
+                                   (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
-
 (set-exec-path-from-shell-PATH)
