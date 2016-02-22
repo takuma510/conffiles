2253b2ddced0bf31fd3874e8feb740e2db335f5a
Takuma Goto <takuma510@gmail.com>
2016-02-22 11:18:10 +0900

    [Emacs] Disable elscreen-display-tab

--- emacs.d/inits/00_global.el
+++ emacs.d/inits/00_global.el
@@ -49,6 +49,9 @@
 (setq elscreen-prefix-key (kbd "C-t"))
 (elscreen-start)
 
+;; settings
+(setq elscreen-display-tab nil)
+
 ;; elscreen-persist
 (require 'elscreen-persist)
 (elscreen-persist-mode 1)
