0075c6cc8f49fa7b660346851009f41867796a21
Takuma Goto <takuma510@gmail.com>
2016-03-05 21:45:47 +0900

    [Emacs] Remove ac-robe-setup

--- emacs.d/inits/21_ruby.el
+++ emacs.d/inits/21_ruby.el
@@ -39,7 +39,7 @@
 (require 'robe)
 (add-hook 'ruby-mode-hook 'robe-mode)
 (autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
-(add-hook 'robe-mode-hook 'ac-robe-setup)
+
 
 ;; company
 (eval-after-load 'company
