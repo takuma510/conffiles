54ee102fbd4aec3ba852777dd36a35665e11db8c
Takuma Goto <takuma510@gmail.com>
2016-03-08 07:24:29 +0900

    [Emacs] Mod. ruby-mode

--- emacs.d/inits/21_ruby.el
+++ emacs.d/inits/21_ruby.el
@@ -30,7 +30,6 @@
 (setq inf-ruby-eval-binding "Pry.toplevel_binding")
 (add-hook 'inf-ruby-mode-hook 'ansi-color-for-comint-mode-on)
 
-
 ;; ruby-block
 (require 'ruby-block)
 (setq ruby-block-highlight-toggle t)
@@ -38,8 +37,8 @@
 ;; robe
 (require 'robe)
 (add-hook 'ruby-mode-hook 'robe-mode)
-(autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
-
+(autoload 'robe-mode
+  "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
 
 ;; company
 (eval-after-load 'company
@@ -47,15 +46,6 @@
 
 ;; style
 (setq ruby-deep-indent-paren-style nil)
-
-;; アクセス修飾子のインデントを下げる
-(push "public"    ruby-block-mid-keywords)
-(push "protected" ruby-block-mid-keywords)
-(push "private"   ruby-block-mid-keywords)
-(setq ruby-block-mid-re (regexp-opt ruby-block-mid-keywords))
-(setq ruby-negative
-      (concat "^[ \t]*\\(\\(" ruby-block-mid-re "\\)\\>\\|"
-              ruby-block-end-re "\\|}\\|\\]\\)"))
 
 ;; ruby-mode-hook
 (defun my-ruby-mode-hooks ()
@@ -69,9 +59,6 @@
 
 ;; Enable linum-mode on ruby-mode
 (add-hook 'ruby-mode-hook #'linum-on)
-
-;; auto-insert
-(define-auto-insert "\\.rb$" "ruby-template.rb")
 
 
 ;;; 21_ruby.el ends here
