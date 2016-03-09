739aefc06c32268471cfcd4f8f87bc3ff4dcf8f1
Takuma Goto <takuma510@gmail.com>
2016-03-09 15:45:27 +0900

    [Emacs] Activate linum on web-mode

--- emacs.d/inits/04_webmode.el
+++ emacs.d/inits/04_webmode.el
@@ -10,4 +10,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(add-hook 'web-mode-hook #'linum-on)
+
+
 ;;; 04_webmode.el ends here
