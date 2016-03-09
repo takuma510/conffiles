7c7e2451c7840b886806e5da00f433d2224a619e
Takuma Goto <takuma510@gmail.com>
2016-03-09 09:13:43 +0900

    [Emacs] Add C-c v kbd

--- emacs.d/inits/14_view.el
+++ emacs.d/inits/14_view.el
@@ -1,12 +1,13 @@
+;;; 14_view.el -- view-mode settings
 
-;;;;  view-mode
+;;; Commentary:
+
+;;; Code:
 
 
-;; C-x C-r でview-modeでファイルを開く
-(setq view-read-only t)
+(setq view-read-only t) ; C-x C-r でview-modeでファイルを開く
 
-
-;; key-bind
+;; knd
 (require 'view)
 (define-key view-mode-map (kbd "/") 'isearch-forward)
 (define-key view-mode-map (kbd "G") 'end-of-buffer)
@@ -14,7 +15,7 @@
 (define-key view-mode-map (kbd "j") 'next-line)
 (define-key view-mode-map (kbd "k") 'previous-line)
 (define-key view-mode-map (kbd "l") 'forward-char)
-
+(global-set-key (kbd "C-c v") 'view-mode)
 
 (require 'viewer)
 ;; 書き込み不能なファイルではviewer-modeから抜けない
@@ -25,3 +26,6 @@
 (setq viewer-modeline-color-unwritable "tomato")
 (setq viewer-modeline-color-view "orange")
 (viewer-change-modeline-color-setup)
+
+
+;;; 14_view.el ends here
