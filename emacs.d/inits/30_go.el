ac57386223182c7d5be7b5da6d9140aa04cc17bc
Takuma Goto <takuma510@gmail.com>
2019-06-04 09:59:40 +0900

    [Fix] broken code

--- emacs.d/inits/30_go.el
+++ emacs.d/inits/30_go.el
@@ -16,12 +16,3 @@
 (defun my/helm-go ()
   (interactive)
   (helm :sources '(my/helm-go-source) :buffer "*helm go*"))
-
-(eval-after-load "go-mode"
-  '(progn
-     (require 'go-autocomplete)
-     (add-hook 'go-mode-hook 'go-eldoc-setup)
-
-     ;; key bindings
-     (define-key go-mode-map (kbd "M-.") 'godef-jump)
-     (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)))
