ef4c64ff162d0c83ba927ab4c662c4a9ada14a66
Takuma Goto <takuma510@gmail.com>
2019-09-28 23:14:36 +0900

    swiper

--- emacs.d/inits/05_ivy-counsel.el
+++ emacs.d/inits/05_ivy-counsel.el
@@ -9,6 +9,8 @@
 (counsel-mode 1)
 ;; M-x, C-x C-fなどのEmacsの基本的な組み込みコマンドをivy版にリマップする
 
+(global-set-key "\C-s" 'swiper)
+
 (global-set-key (kbd "C-;") 'counsel-recentf)
 (global-set-key (kbd "C-c g") 'counsel-git)
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
