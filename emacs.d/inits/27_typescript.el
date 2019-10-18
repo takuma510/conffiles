d398eabc4ac1def8c556fe75db7a5f935c0fff25
Takuma Goto <takuma510@gmail.com>
2019-10-18 21:10:17 +0900

    tsx

---
+++ emacs.d/inits/27_typescript.el
@@ -0,0 +1,11 @@
+;;; 27_typescript.el --- typescript settings
+
+;;; Commentary:
+
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.tsx$" . typescript-mode))
+
+(provide '27_typescript)
+
+;;; 27_typescript.el ends here
